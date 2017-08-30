c36954f04ab0872a228d9c778ec311f85f17c714
Cosmin Cojocar <cosmc@microsoft.com>
2017-08-30 16:00:56 +0200

    Add the CHACHA20 to good ciphers in modern tls check

--- rules/tls.go
+++ rules/tls.go
@@ -130,8 +130,10 @@ func NewModernTlsCheck(conf map[string]interface{}) (gas.Rule, []ast.Node) {
 		goodCiphers: []string{
 			"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
 			"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+			"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
 			"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
 			"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+			"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
 		},
 	}, []ast.Node{(*ast.CompositeLit)(nil)}
 }
