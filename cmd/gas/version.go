21e1e84e64d80e07e617c0f1f99a6e6695b6e894
Cosmin Cojocar <cosmc@microsoft.com>
2018-02-21 11:32:50 +0100

    Inject the version, git tag and build date as build variables

--- cmd/gas/version.go
+++ cmd/gas/version.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
+var (
+	Version, GitTag, BuildDate string
 )
-
-const (
-	major = 2
-	minor = 0
-	patch = 0
-	tag   = ""
-)
-
-// Version builds a semantic version
-func Version() string {
-	version := fmt.Sprintf("%d.%d.%d", major, minor, patch)
-	if tag != "" {
-		version = fmt.Sprintf("%s-%s", version, tag)
-	}
-	return version
-}
