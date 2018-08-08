8dfa8dc0150755c479ed115c1747574f0af55c43
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-08-08 16:41:34 +0200

    Update README

--- rules/rulelist.go
+++ rules/rulelist.go
@@ -80,7 +80,7 @@ func Generate(filters ...RuleFilter) RuleList {
 		{"G305", "File path traversal when extracting zip archive", NewArchive},
 
 		// crypto
-		{"G401", "Detect the usage of DES, RC4, or MD5", NewUsesWeakCryptography},
+		{"G401", "Detect the usage of DES, RC4, MD5 or SHA1", NewUsesWeakCryptography},
 		{"G402", "Look for bad TLS connection settings", NewIntermediateTLSCheck},
 		{"G403", "Ensure minimum RSA key length of 2048 bits", NewWeakKeyStrength},
 		{"G404", "Insecure random number source (rand)", NewWeakRandCheck},
