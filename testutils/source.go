87782362ba982abba94b9d81ac3ad206469d157d
Ben Bytheway <bbytheway@gmail.com>
2019-06-12 13:53:17 -0600

    One approach for fixing the false positive identified in #325.

--- testutils/source.go
+++ testutils/source.go
@@ -437,6 +437,26 @@ func main(){
 	}
 	defer rows.Close()
 }`}, 0}, {[]string{`
+// false positive
+package main
+import (
+	"database/sql"
+	"fmt"
+)
+
+const Table = "foo"
+func main(){
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		panic(err)
+	}
+	q := fmt.Sprintf("SELECT * FROM %s where id = 1", Table)
+	rows, err := db.Query(q)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+}`}, 0}, {[]string{`
 package main
 import (
 	"fmt"
