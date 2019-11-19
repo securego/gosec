df484bfa9e9ff3f34e09869a289d1568f2ee4e90
Lars Lehtonen <lars.lehtonen@gmail.com>
2019-11-19 02:41:25 -0800

    cmd/tlsconfig: remove support for deprecated tls.VersionSSL30 (#412)
    * cmd/tlsconfig: build tags to deprecate tls.VersionSSL30 from go1.14
    
    * cmd/tlsconfig: build tags to turn off TLSv1.3 in go1.11

---
+++ cmd/tlsconfig/tls_version_go12_go13.go
@@ -0,0 +1,33 @@
+// +build go1.12,!go1.14
+
+// This file can be removed once go1.13 is no longer supported
+
+package main
+
+import (
+	"crypto/tls"
+	"sort"
+)
+
+func mapTLSVersions(tlsVersions []string) []int {
+	var versions []int
+	for _, tlsVersion := range tlsVersions {
+		switch tlsVersion {
+		case "TLSv1.3":
+			versions = append(versions, tls.VersionTLS13)
+		case "TLSv1.2":
+			versions = append(versions, tls.VersionTLS12)
+		case "TLSv1.1":
+			versions = append(versions, tls.VersionTLS11)
+		case "TLSv1":
+			versions = append(versions, tls.VersionTLS10)
+		case "SSLv3":
+			// unsupported from go1.14
+			versions = append(versions, tls.VersionSSL30)
+		default:
+			continue
+		}
+	}
+	sort.Ints(versions)
+	return versions
+}
