7bc5ae9433f9e9a41767a484ffbc26e5cfca9755
L. Pivarc <lpivarc@redhat.com>
2020-09-24 14:37:35 +0200

    Make ordering stable
    Signed-off-by: L. Pivarc <lpivarc@redhat.com>

--- cmd/gosec/sort_issues.go
+++ cmd/gosec/sort_issues.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/securego/gosec/v2"
 )
+
+// handle ranges
+func extractLineNumber(s string) int {
+	lineNumber, _ := strconv.Atoi(strings.Split(s, "-")[0])
+	return lineNumber
+
+}
 
 type sortBySeverity []*gosec.Issue
 
 func (s sortBySeverity) Len() int { return len(s) }
 
-func (s sortBySeverity) Less(i, j int) bool { return s[i].Severity > s[j].Severity }
+func (s sortBySeverity) Less(i, j int) bool {
+	if s[i].Severity == s[j].Severity {
+		if s[i].What == s[j].What {
+			if s[i].File == s[j].File {
+				return extractLineNumber(s[i].Line) > extractLineNumber(s[j].Line)
+			}
+			return s[i].File > s[j].File
+		}
+		return s[i].What > s[j].What
+	}
+	return s[i].Severity > s[j].Severity
+}
 
 func (s sortBySeverity) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
 
