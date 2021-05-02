97fe819259c42efa171b33f2751286deedf294a4
MOREL Matthieu <matthieu.morel@cnp.fr>
2021-05-02 17:42:49 +0200

    missed warnings

--- output/formatter_test.go
+++ output/formatter_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func createIssueWithFileWhat(file, what string) *gosec.Issue {
-	issue := createIssue("i1", gosec.GetCwe("G101"))
+	issue := createIssue("i1", gosec.GetCwe("G101", "G101 Name"))
 	issue.File = file
 	issue.What = what
 	return &issue
