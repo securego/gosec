32a5235a5287c9137d006f4a8bf84b8a2756fc37
MOREL Matthieu <matthieu.morel@cnp.fr>
2021-05-03 10:29:12 +0200

    Add comments

--- cwe/types.go
+++ cwe/types.go
@@ -1,5 +1,7 @@
+// This types are based on http://cwe.mitre.org/data/xsd/cwe_schema_v6.4.xsd
 package cwe
 
+// Weakness defines a CWE weakness
 type Weakness struct {
 	ID          string
 	Name        string
