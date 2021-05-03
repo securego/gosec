34a443132bd524fc237d7383a8feee6b8ff981d7
MOREL Matthieu <matthieu.morel@cnp.fr>
2021-05-03 11:04:58 +0200

    useless blank line

--- cwe/data.go
+++ cwe/data.go
@@ -7,8 +7,7 @@ var data = map[string]Weakness{
 		Name:        "Incorrect Access of Indexable Resource ('Range Error')",
 	},
 	"190": {
-		ID: "190",
-
+		ID:          "190",
 		Description: "The software performs a calculation that can produce an integer overflow or wraparound, when the logic assumes that the resulting value will always be larger than the original value. This can introduce other weaknesses when the calculation is used for resource management or execution control.",
 		Name:        "Integer Overflow or Wraparound",
 	},
