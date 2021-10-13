318cd3a74988d299df08c07e7d9ff87b08c1dbef
HeyCamCam <ansiblenotify+gh@gmail.com>
2021-10-13 07:35:35 -0400

    spelling fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Expand the arguments into an array of strings. This is requires because the GitHub action
+# Expand the arguments into an array of strings. This is required because the GitHub action
 # provides all arguments concatenated as a single string.
 ARGS=("$@")
 
