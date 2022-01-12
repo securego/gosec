83e31e83ff5255d49c9e38a5795c17db0faf16f5
kaiili <35690781+kaiili@users.noreply.github.com>
2022-01-12 08:45:11 +0800

    rename variable,remove useless comment

--- rules/sql.go
+++ rules/sql.go
@@ -261,14 +261,12 @@ func (s *sqlStrFormat) Match(n ast.Node, ctx *gosec.Context) (*gosec.Issue, erro
 	switch stmt := n.(type) {
 	case *ast.AssignStmt:
 		for _, expr := range stmt.Rhs {
-			// when expr is CallExpr and Call.Fun.X is CallExpr ,check Call.Fun.X
-			if Call, ok := expr.(*ast.CallExpr); ok {
-				Selector, ok := Call.Fun.(*ast.SelectorExpr)
+			if call, ok := expr.(*ast.CallExpr); ok {
+				selector, ok := call.Fun.(*ast.SelectorExpr)
 				if !ok {
 					continue
 				}
-				sqlQueryCall, ok := Selector.X.(*ast.CallExpr)
-				// checkQuery when success get sqlQuery CallExpr and Selector contains Callexpr
+				sqlQueryCall, ok := selector.X.(*ast.CallExpr)
 				if ok && s.ContainsCallExpr(sqlQueryCall, ctx) != nil {
 					issue, err := s.checkQuery(sqlQueryCall, ctx)
 					if err == nil && issue != nil {
