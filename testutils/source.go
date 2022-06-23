b0f3e78e07d26ff0156a493fb8232f0de5b8ce15
云微 <1067852565@qq.com>
2022-06-23 20:58:13 +0800

    fix ReadTimeout for G112 rule

--- testutils/source.go
+++ testutils/source.go
@@ -1050,6 +1050,29 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 		`}, 0, gosec.NewConfig()},
+		{[]string{`
+		package main
+
+		import (
+			"fmt"
+			"time"
+			"net/http"
+		)
+		
+		func main() {
+			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+			})
+			server := &http.Server{
+				Addr:              ":1234",
+				ReadTimeout:  	   1 * time.Second,
+			}
+			err := server.ListenAndServe()
+			if err != nil {
+				panic(err)
+			}
+		}
+		`}, 0, gosec.NewConfig()},
 	}
 
 	// SampleCodeG113 - Usage of Rat.SetString in math/big with an overflow
