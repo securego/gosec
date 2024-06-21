fc0957f6a3b0d1c0db43ea72266daab41143c4d9
Dimitar Banchev <dimitar.banchev@mercedes-benz.com>
2024-06-21 15:14:37 +0200

    Minor changes
    * Renamed the file responsible for rule G401
    * Removed copyright of HP from the new rule

--- rules/weakcryptohash.go
+++ rules/weakcryptohash.go
@@ -1,5 +1,3 @@
-// (c) Copyright 2016 Hewlett Packard Enterprise Development LP
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
