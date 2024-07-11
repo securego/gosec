7a4df24df1ab6e1a0747ee82d75ed33b0a69265a
Alex Gartner <git@agartner.com>
2024-07-11 12:08:11 -0700

    Fix multifile ignores

--- testutils/g115_samples.go
+++ testutils/g115_samples.go
@@ -115,4 +115,43 @@ func main() {
     fmt.Println(b)
 }
 	`}, 1, gosec.NewConfig()},
+	{[]string{
+		`
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a uint = math.MaxUint
+	// #nosec G115
+	b := int64(a)
+	fmt.Println(b)
+}
+		`,
+	}, 0, gosec.NewConfig()},
+	{[]string{
+		`
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+    var a uint = math.MaxUint
+	// #nosec G115
+    b := int64(a)
+    fmt.Println(b)
+}
+	`, `
+package main
+
+func ExampleFunction() {
+}
+`,
+	}, 0, gosec.NewConfig()},
 }
