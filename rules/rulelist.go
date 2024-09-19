0f54e77923ea96fd3092028bc65ed05cd36b365b
Laurent Demailly <ldemailly@gmail.com>
2024-09-19 10:40:39 -0700

    Fix running single analyzer which isn't a rule bug

--- rules/rulelist.go
+++ rules/rulelist.go
@@ -14,7 +14,9 @@
 
 package rules
 
-import "github.com/securego/gosec/v2"
+import (
+	"github.com/securego/gosec/v2"
+)
 
 // RuleDefinition contains the description of a rule and a mechanism to
 // create it.
