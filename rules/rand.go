d70cac375c74052389aed6f2209fd2fd942dc6ab
リヴァイ <184781722+Haiagari@users.noreply.github.com>
2026-09-14 06:50:42 -0500

    fix(G404): ignore non-cryptographic Shuffle calls (#1742)
    * fix(G404): ignore non-cryptographic Shuffle calls
    
    * test(G404): cover Shuffle exclusions

--- rules/rand.go
+++ rules/rand.go
@@ -31,9 +31,9 @@ func NewWeakRandCheck(id string, _ gosec.Config) (gosec.Rule, []ast.Node) {
 		"Use of weak random number generator (math/rand or math/rand/v2 instead of crypto/rand)",
 		issue.High, issue.Medium)}
 	rule.AddAll("math/rand", "New", "Read", "ExpFloat64", "Float32", "Float64", "Int", "Int31", "Int31n",
-		"Int63", "Int63n", "Intn", "NormFloat64", "Perm", "Shuffle", "Uint32", "Uint64")
+		"Int63", "Int63n", "Intn", "NormFloat64", "Perm", "Uint32", "Uint64")
 	rule.AddAll("math/rand/v2", "New", "ExpFloat64", "Float32", "Float64", "Int", "Int32", "Int32N",
-		"Int64", "Int64N", "IntN", "N", "NormFloat64", "Perm", "Shuffle", "Uint", "Uint32", "Uint32N", "Uint64", "Uint64N", "UintN")
+		"Int64", "Int64N", "IntN", "N", "NormFloat64", "Perm", "Uint", "Uint32", "Uint32N", "Uint64", "Uint64N", "UintN")
 
 	return rule, []ast.Node{(*ast.CallExpr)(nil)}
 }
