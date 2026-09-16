a537d679eda6afd515f70711f7be0c90806024b1
Jasur Gulyamov <gulamovzavohir02@gmail.com>
2026-09-16 19:44:55 +0400

    fix(G403): resolve constant RSA key sizes
    Signed-off-by: Jasur Gulyamov <gulamovzavohir02@gmail.com>

--- testutils/g403_samples.go
+++ testutils/g403_samples.go
@@ -22,4 +22,44 @@ func main() {
 	fmt.Println(pvk)
 }
 `}, 1, gosec.NewConfig()},
+	{[]string{`
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+const weakKeyBits = 1024
+
+func main() {
+	_, _ = rsa.GenerateKey(rand.Reader, weakKeyBits)
+}
+`}, 1, gosec.NewConfig()},
+	{[]string{`
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+func main() {
+	_, _ = rsa.GenerateKey(rand.Reader, 1<<10)
+}
+`}, 1, gosec.NewConfig()},
+	{[]string{`
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+const secureKeyBits = 2048
+
+func main() {
+	_, _ = rsa.GenerateKey(rand.Reader, secureKeyBits)
+}
+`}, 0, gosec.NewConfig()},
 }
